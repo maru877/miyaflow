e8a7bf7e8ca375a43cc25b3cb2046c46c3bba9dc
Yuki Miyata <hq3p15ofcv@gmail.com>
2017-06-26 10:35:03 +0900

    バリデーションを設定

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,6 @@ class Answer < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :question, optional: true
   has_many :evaluations, dependent: :destroy
+
+  validates :body, presence: true
 end
